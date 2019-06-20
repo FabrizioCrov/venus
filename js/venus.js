9c95d59ab34c12382b15e5d0903fe10f80edca77
Fabrizio Crovetto <51926897+FabrizioCrov@users.noreply.github.com>
2019-06-19 19:34:46 -0500

    Add files via upload

---
+++ js/venus.js
@@ -0,0 +1,48 @@
+/* Galeria */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption1");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active1", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active1";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
+}
+
+/* Mostrar al bajar */
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+      document.getElementById("subir").style.display = "block";
+      document.getElementById("icon-bar").style.display = "block";
+    } else {
+      document.getElementById("subir").style.display = "none";
+      document.getElementById("icon-bar").style.display = "none";
+    }
+  }
+
+/* boton subir */
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
